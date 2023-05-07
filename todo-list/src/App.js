9e5cf656bb6c9621141a4f9e7d230a04f204961b
Cate <anncatherinedeguia@gmail.com>
2023-05-07 16:31:58 +0800

    Add filter function

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -5,6 +5,17 @@ import FilterButton from "./components/FilterButton";
 import { nanoid } from "nanoid";
 
 
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
 
@@ -40,7 +51,11 @@ function App(props) {
     setTasks(updatedTasks);
   }
 
-  const taskList = tasks.map((task) => (
+  const [filter, setFilter] = useState('All');
+
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map((task) => (
     <Todo
       id={task.id}
       name={task.name}
@@ -52,6 +67,15 @@ function App(props) {
     />
   ));
 
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
@@ -60,9 +84,7 @@ function App(props) {
       <h1>ToDue</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+      {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
